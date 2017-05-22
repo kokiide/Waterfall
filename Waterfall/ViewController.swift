2e0eb41e12686053979d27aabe8094f68f2f7edc
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-05-22 13:01:55 +0900

    Initial Commit

---
+++ Waterfall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waterfall
+//
+//  Created by Koki Ide on 2017/05/22.
+//  Copyright © 2017 Koki Ide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
